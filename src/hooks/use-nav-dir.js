63b6115a8d8a9cbb30cbe34fbc6abac3cf3eb371
Nick Lazaga <nicklazaga@berkeley.edu>
2023-05-25 16:11:07 -0700

    fix: indexing error

--- src/hooks/use-nav-dir.js
+++ src/hooks/use-nav-dir.js
@@ -5,7 +5,7 @@ export default function useNavDir() {
   const pathname = usePathname();
   if (pathname === "/") return undefined;
 
-  const startInd = 0;
+  const startInd = 1;
   const endInd = pathname.indexOf("/", startInd + 1);
 
   return pathname.slice(startInd, endInd === -1 ? pathname.length : endInd);
