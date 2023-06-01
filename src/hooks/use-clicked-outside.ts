ac7667e0d27860466a7265a97cadd5bab2c48c51
Nick Lazaga <nicklazaga@berkeley.edu>
2023-05-31 18:43:37 -0700

    Added documentation.

--- src/hooks/use-clicked-outside.ts
+++ src/hooks/use-clicked-outside.ts
@@ -6,11 +6,30 @@ function assertIsNode(event: EventTarget | null): asserts event is Node {
   }
 }
 
-export default function useClickedOutside(
-  ref: RefObject<HTMLDivElement> | null,
-  dependency: any,
-  callback: CallableFunction
-) {
+interface useClickedOutsideProps {
+  /**
+   * A ref to the DOM element that the click event target will be tested
+   * against.
+   */
+  ref: RefObject<HTMLDivElement> | null;
+
+  /**
+   * When truthy, signals the hook to add the event listener.
+   * Defaults to true (adds the event listener immediately).
+   */
+  dependency?: any;
+
+  /**
+   * Called when the event listener is active and the "Escape" key is pressed.
+   */
+  callback: CallableFunction;
+}
+
+export default function useClickedOutside({
+  ref,
+  dependency = true,
+  callback,
+}: useClickedOutsideProps) {
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
       assertIsNode(event.target);
