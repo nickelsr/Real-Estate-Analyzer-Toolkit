c79f859df45ed414a4c1eaea84e3d347a204db5c
Nick Lazaga <nicklazaga@berkeley.edu>
2023-05-31 17:04:45 -0700

    updated re-exports to include new hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+import { default as useBreadcrumbs } from "./use-breadcrumbs";
+import { default as useNavDir } from "./use-nav-dir";
+import { default as useClickedOutside } from "./use-clicked-outside";
+import { default as usePressedEscape } from "./use-pressed-escape";
+
+export { useBreadcrumbs, useNavDir, useClickedOutside, usePressedEscape };
