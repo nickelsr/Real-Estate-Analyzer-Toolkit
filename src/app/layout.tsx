10579e339f95c285316d60220c8a072634671b5d
Nick Lazaga <nicklazaga@berkeley.edu>
2023-05-31 19:34:26 -0700

    Updated import for global css.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
-import "./globals.css";
 import { Inter } from "next/font/google";
+import "@styles/globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
